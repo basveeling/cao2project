efb58df36c75f7b1f419067a8b687a59c8a41c30
Bas Veeling <basveeling@gmail.com>
2012-12-06 16:33:10 +0100

    updated decoder function

--- utilities.vhd
+++ utilities.vhd
@@ -36,11 +36,15 @@ PACKAGE BODY utilities IS
     VARIABLE index : natural;
   BEGIN
     index := to_integer(unsigned(s));
-    IF index<38 THEN
+    IF index<8 THEN
       RETURN index;
-    ELSE
+    ELSIF index > 31 THEN
+      RETURN index - 24;
+    ELSIF index > 37
       REPORT "decoder index out of range" SEVERITY warning;
       RETURN 0; -- %r0 is read only, therefore this is a safe return value
+    ELSE
+      -- RETURN INDEX MET schuifshit
     END IF;
   END decoder;
 
